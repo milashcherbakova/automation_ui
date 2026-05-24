b98065f9d52bff298ac08f63a09966aa1254ef75
mila <m14600871@gmail.com>
2026-05-24 18:41:51 +0300

    initial commit

---
+++ playwright_auth.py
@@ -0,0 +1,53 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    # Открываем браузер и создаем новую страницу
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    # Переходим на страницу входа
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+    # Заполняем поле email
+    email_input = page.get_by_test_id('login-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+
+    # Заполняем поле пароль
+    password_input = page.get_by_test_id('login-form-password-input').locator('input')
+    password_input.fill("password")
+
+    # Нажимаем на кнопку Login
+    login_button = page.get_by_test_id('login-page-login-button')
+    login_button.click()
+
+    # Проверяем, что появилось сообщение об ошибке
+    wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+
+    # Задержка для наглядности выполнения теста (не рекомендуется использовать в реальных автотестах)
+    page.wait_for_timeout(5000)
+
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text('Dashboard')
