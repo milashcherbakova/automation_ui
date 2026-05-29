f24e9a828b3ac38fc7e3f0bcd5c039202ed52803
mila <m14600871@gmail.com>
2026-05-29 16:15:07 +0300

    initial ccommit

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,13 +1,12 @@
 from playwright.sync_api import sync_playwright
 from urllib3.util import timeout
 
-# Открываем браузер с использованием Playwright
 with sync_playwright() as playwright:
-    # Запускаем Chromium браузер в обычном режиме (не headless)
+
     browser = playwright.chromium.launch(headless=False)
-    # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
+
     context = browser.new_context()
-    # Открываем новую страницу в рамках контекста
+
     page = context.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
@@ -24,6 +23,20 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
+
     context.storage_state(path="browser-state.json")
-page.wait_for_timeout(5000)
+
+
+
+from playwright.sync_api import sync_playwright
+
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(5000)
